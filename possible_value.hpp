6a4345682c2288489a0b417c621b82383f8e72fe
Duke Nguyen <duke7295@gmail.com>
2022-12-15 13:16:24 +0100

    Refactor and cleanup

--- possible_value.hpp
+++ possible_value.hpp
@@ -1,3 +1,6 @@
+#ifndef SUDOKU_SOLVER__POSSIBLE_VALUE_HPP_
+#define SUDOKU_SOLVER__POSSIBLE_VALUE_HPP_
+
 #include <array>
 #include <iostream>
 
@@ -7,23 +10,29 @@ using std::cout, std::endl;
 static const unsigned char SIZE = 9;
 using P_VAL = unsigned short;
 
-struct possible_value
-{
-    int _N;
-    array<P_VAL, SIZE + 1> _possibilities;
 
+class possible_value
+{
+private:
+    int N_;
+    array<P_VAL, SIZE + 1> possibilities_;
+
+public:
     possible_value();
     ~possible_value();
 
+    P_VAL get_N() const;
+    P_VAL get_val() const;
+    void print() const;
+    bool check_val(const P_VAL &val) const;
+
     void remove_val(const P_VAL &val);
     void clear();
-    void print() const;
-    P_VAL get_val() const;
 };
 
 possible_value::possible_value() {
-    _N = SIZE;
-    _possibilities.fill(1);
+    N_ = SIZE;
+    possibilities_.fill(1);
 }
 
 possible_value::~possible_value() {}
@@ -33,34 +42,43 @@ void possible_value::remove_val(const P_VAL &val) {
         cout << "Error: Invalid value (not in range 1 to 9)." << endl;
         return;
     }
-    if (_possibilities[val] == 0)
+    if (possibilities_[val] == 0)
         return;
-    if (_possibilities[val] == 1) {
-        _N--;
-        _possibilities[val] = 0;
+    if (possibilities_[val] == 1) {
+        N_--;
+        possibilities_[val] = 0;
     }
 }
 
 void possible_value::clear() {
-    _N = 0;
-    _possibilities.fill(0);
+    N_ = 0;
+    possibilities_.fill(0);
 }
 
 void possible_value::print() const {
-    cout << _N << " possible values:";
+    cout << N_ << " possible values:";
     for (int i = 1; i <= SIZE; i++) {
-        if (_possibilities[i])        
+        if (possibilities_[i])        
             cout << " " << i;
     }
-    cout << endl;    
+    cout << endl;
 }
 
 P_VAL possible_value::get_val() const {
-    if (_N > 1)
+    if (N_ > 1)
         return 0;
     for (int i = 1; i <= SIZE; i++) {
-        if (_possibilities[i])
+        if (possibilities_[i])
             return i;
     }
     return 0;    
 }
+
+bool possible_value::check_val(const P_VAL &val) const
+{
+    return possibilities_[val] == 1;
+}
+
+P_VAL possible_value::get_N() const { return N_; }
+
+#endif  // SUDOKU_SOLVER__POSSIBLE_VALUE_HPP_
--- sup_print.hpp
+++ sup_print.hpp
@@ -1,3 +1,6 @@
+#ifndef SUDOKU_SOLVER__SUP_PRINT_HPP_
+#define SUDOKU_SOLVER__SUP_PRINT_HPP_
+
 #include <vector>
 #include <iostream>
 
@@ -5,6 +8,7 @@ using std::cin, std::cout, std::endl, std::ostream;
 using std::vector;
 
 // Supporting function for: printing and logging
+
 template <typename T>
 ostream & operator << (ostream& os, const vector<T>& vec)
 {
@@ -13,3 +17,7 @@ ostream & operator << (ostream& os, const vector<T>& vec)
         os << elem << " ";
     return os;
 }
+
+
+
+#endif  // SUDOKU_SOLVER__SUP_PRINT_HPP_
