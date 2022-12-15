a3261a89a83636b4a14b9f1371dad2d0567a11ef
Duke Nguyen <duke7295@gmail.com>
2022-12-15 13:16:14 +0100

    Fix bugs and refactor

--- sudoku.hpp
+++ sudoku.hpp
@@ -1,9 +1,12 @@
+#ifndef SUDOKU_SOLVER__SUDOKU_HPP_
+#define SUDOKU_SOLVER__SUDOKU_HPP_
+
 #include <iostream>
 #include <vector>
 #include <array>
 #include <algorithm>
 
-#include "sup_vector.hpp"
+#include "sup_print.hpp"
 #include "possible_value.hpp"
 
 #define LOGGING true
@@ -16,6 +19,8 @@ using SUDOKU_ID = unsigned short;
 
 // Size const
 static const unsigned char S = 9;
+static const SUDOKU_VAL VALID = 10;
+static const SUDOKU_VAL ERROR = 20;
 const unsigned char S_MAP = S * S;
 
 // IDs of cells in the same row
@@ -64,14 +69,24 @@ class Sudoku
 public:
     Sudoku();
     ~Sudoku();
-    // template <typename T>
-    // Sudoku(const T &array[]);
 
-    void get_map();
-    void solve();
-    void print() const;
-    void print_possible_values() const;
-    // void print(int map[]);
+    static SUDOKU_ID id2row_id(const SUDOKU_ID &id);
+    static SUDOKU_ID id2column_id(const SUDOKU_ID &id);
+    static SUDOKU_ID id2block_id(const SUDOKU_ID &id);
+
+    void get_map();                     // get map from std::cin
+    void get_map(SUDOKU_VAL array[]);   // get map from an array
+
+    int get_num_found() const;          // get number of found cells
+    int get_num_unchecked() const;      // get number of checked cells
+    int get_num_not_found() const;      // get number of unfound cells
+
+    // Set value to a cell
+    void set_cell_value(const SUDOKU_ID &id, const SUDOKU_VAL &val);
+    SUDOKU_VAL get_cell_value(const SUDOKU_ID &id) const;
+
+    void print() const;                 // print the map
+    void print_possible_values() const; // print cells' possible values
 
     // Update possible values of other cells based on known cells
     void update_possible_values();
@@ -79,34 +94,64 @@ public:
     void advanced_update();
     // Fill cells with only 1 possible values
     void fill_certain_values();
-
-    static SUDOKU_ID id2row_id(const SUDOKU_ID &id);
-    static SUDOKU_ID id2column_id(const SUDOKU_ID &id);
-    static SUDOKU_ID id2block_id(const SUDOKU_ID &id);
+    void solve();
 
 private:
-    SUDOKU_VAL _map[S_MAP];
-    vector<SUDOKU_ID> _ids_found;
-    vector<SUDOKU_ID> _ids_not_found;
-    array<possible_value, S_MAP> _possible_vals;
+    SUDOKU_VAL map_[S_MAP];
+    vector<SUDOKU_ID> ids_found_;       // id of known cells
+    vector<SUDOKU_ID> ids_not_found_;   // id of unknown cells
+    // id of known cells but haven't used for update
+    vector<SUDOKU_ID> ids_unchecked_;
+    array<possible_value, S_MAP> possible_vals_;
 };
 
 Sudoku::Sudoku() {
     for (int i = 0; i < S_MAP; i++)
-        _map[i] = 0;
+        map_[i] = 0;
+    ids_found_.clear();
+    ids_unchecked_.clear();
+    ids_not_found_.clear();
+    for (SUDOKU_ID id = 0; id < S_MAP; id++)
+        ids_not_found_.push_back(id);
 }
 
 Sudoku::~Sudoku() {}
 
 void Sudoku::get_map() {
-    _ids_found.clear();
-    _ids_not_found.clear();
+    ids_found_.clear();
+    ids_not_found_.clear();
+    ids_unchecked_.clear();
     for (int i = 0; i < S_MAP; i++) {
-        cin >> _map[i];
-        if (_map[i] == 0)
-            _ids_not_found.emplace_back(i);
-        else
-            _ids_found.emplace_back(i);
+        cin >> map_[i];
+        if (map_[i] < 0 || map_[i] > 9) {
+            cout << "Invalid value for a cell: " << map_[i] << endl;
+            break;
+        }
+        if (map_[i] == 0)
+            ids_not_found_.emplace_back(i);
+        else {
+            ids_found_.emplace_back(i);
+            ids_unchecked_.emplace_back(i);
+        }
+    }
+}
+
+void Sudoku::get_map(SUDOKU_VAL array[]) {
+    ids_found_.clear();
+    ids_not_found_.clear();
+    ids_unchecked_.clear();
+    for (int i = 0; i < S_MAP; i++) {
+        map_[i] = array[i];
+        if (map_[i] < 0 || map_[i] > 9) {
+            cout << "Invalid value for a cell: " << map_[i] << endl;
+            break;
+        }
+        if (map_[i] == 0)
+            ids_not_found_.emplace_back(i);
+        else {
+            ids_found_.emplace_back(i);
+            ids_unchecked_.emplace_back(i);
+        }
     }
 }
 
@@ -118,10 +163,10 @@ void Sudoku::print() const {
             cout << i/9 + 1;
         if (i % 3 == 0)
             cout << "| ";
-        if (_map[i] == 0)
+        if (map_[i] == 0)
             cout << "  ";
         else
-            cout << _map[i] << " ";
+            cout << map_[i] << " ";
         if (i % 9 == 8)
             cout << "|" << endl;
         if (i % 27 == 26)
@@ -130,17 +175,33 @@ void Sudoku::print() const {
 }
 
 void Sudoku::print_possible_values() const {
-    for (auto id_not_found : _ids_not_found) {
+    for (auto id_not_found : ids_not_found_) {
         cout << "Cell " << id_not_found << " with ";
-        _possible_vals[id_not_found].print();
+        possible_vals_[id_not_found].print();
     }
 }
 
-SUDOKU_ID Sudoku::id2row_id(const SUDOKU_ID &id) { return id / S; }
+SUDOKU_ID Sudoku::id2row_id(const SUDOKU_ID &id) {
+    if (id < 0 || id > 80) {
+        cout << "Invalid value for id (Out of range)." << endl;
+        return ERROR;
+    }
+    return id / S;
+}
 
-SUDOKU_ID Sudoku::id2column_id(const SUDOKU_ID &id) { return id % S; }
+SUDOKU_ID Sudoku::id2column_id(const SUDOKU_ID &id) {
+    if (id < 0 || id > 80) {
+        cout << "Invalid value for id (Out of range)." << endl;
+        return ERROR;
+    }
+    return id % S;
+}
 
 SUDOKU_ID Sudoku::id2block_id(const SUDOKU_ID &id) {
+    if (id < 0 || id > 80) {
+        cout << "Invalid value for id (Out of range)." << endl;
+        return ERROR;
+    }
     return (id / 27) * 3 + (id % 9) / 3;
 }
 
@@ -152,32 +213,33 @@ void Sudoku::update_possible_values() {
 #if LOGGING
     cout << "Update possible values ..." << endl;
 #endif
-    for (SUDOKU_ID id_found : _ids_found) {
-        _possible_vals[id_found].clear();
-        SUDOKU_VAL val = _map[id_found];
-        SUDOKU_ID id_row = id2row_id(id_found);
-        SUDOKU_ID id_column = id2column_id(id_found);
-        SUDOKU_ID id_block = id2block_id(id_found);
+    for (SUDOKU_ID id_unchecked : ids_unchecked_) {
+        possible_vals_[id_unchecked].clear();
+        SUDOKU_VAL val = map_[id_unchecked];
+        SUDOKU_ID id_row = id2row_id(id_unchecked);
+        SUDOKU_ID id_column = id2column_id(id_unchecked);
+        SUDOKU_ID id_block = id2block_id(id_unchecked);
         for (SUDOKU_ID id : ROW_IDS[id_row]) {
-            if (id == id_found)
+            if (id == id_unchecked)
                 continue;
-            _possible_vals[id].remove_val(val);
+            possible_vals_[id].remove_val(val);
         }
         for (SUDOKU_ID id : COLUMN_IDS[id_column]) {
-            if (id == id_found)
+            if (id == id_unchecked)
                 continue;
-            _possible_vals[id].remove_val(val);
+            possible_vals_[id].remove_val(val);
         }
         for (SUDOKU_ID id : BLOCK_IDS[id_block]) {
-            if (id == id_found)
+            if (id == id_unchecked)
                 continue;
-            _possible_vals[id].remove_val(val);
+            possible_vals_[id].remove_val(val);
         }
     }
 #if LOGGING
-    cout << _ids_found.size() << " value(s) is/are used for update." << endl;
+    cout << ids_found_.size() << " value(s) is/are used for update." << endl;
 #endif
-    _ids_found.clear();
+    if (ids_found_.size())
+        ids_found_.clear();
 }
 
 /**
@@ -196,9 +258,9 @@ void Sudoku::advanced_update() {
             id_c = 10;
             // Check whether the value lies in one specific row/col
             for (SUDOKU_ID id : BLOCK_IDS[id_b]) {
-                if (_map[id])   // Consider only unknown cells
+                if (map_[id])   // Consider only unknown cells
                     continue;
-                if (_possible_vals[id]._possibilities[val]) {
+                if (possible_vals_[id].check_val(val)) {
                     if (id_r == 10)
                         id_r = id2row_id(id);
                     else if (id_r < 10) {
@@ -216,22 +278,22 @@ void Sudoku::advanced_update() {
             // If yes, unknown cells of the same row/col can't have it
             if (id_r < 10) {
                 for (SUDOKU_ID id : ROW_IDS[id_r]) {
-                    if (_map[id])   // Consider only unknown cells
+                    if (map_[id])   // Consider only unknown cells
                         continue;
                     // Consider only cells of other block
                     if (id2block_id(id) == id_b)
                         continue;
-                    _possible_vals[id].remove_val(val);
+                    possible_vals_[id].remove_val(val);
                 }
             }
             if (id_c < 10) {
                 for (SUDOKU_ID id : COLUMN_IDS[id_c]) {
-                    if (_map[id])   // Consider only unknown cells
+                    if (map_[id])   // Consider only unknown cells
                         continue;
                     // Consider only cells of other block
                     if (id2block_id(id) == id_b)
                         continue;
-                    _possible_vals[id].remove_val(val);
+                    possible_vals_[id].remove_val(val);
                 }
             }
         }
@@ -239,35 +301,81 @@ void Sudoku::advanced_update() {
 }
 
 void Sudoku::fill_certain_values() {
-    for (SUDOKU_ID id : _ids_not_found) {
-        if (_possible_vals[id]._N != 1)
+    vector<std::vector<SUDOKU_ID>::iterator> to_delete;
+    for (SUDOKU_ID id : ids_not_found_) {
+        if (possible_vals_[id].get_N() != 1)
             continue;
 #if LOGGING
         cout << "Fill cell " << id
-             << " with " << _possible_vals[id].get_val() << endl;
+             << " with " << possible_vals_[id].get_val() << endl;
 #endif
-        _map[id] = _possible_vals[id].get_val();
-        _ids_found.push_back(id);
-        _ids_not_found.erase(find(begin(_ids_not_found), end(_ids_not_found), id));
+        map_[id] = possible_vals_[id].get_val();
+        ids_found_.push_back(id);
+        ids_unchecked_.push_back(id);
+        to_delete.push_back(find(begin(ids_not_found_), end(ids_not_found_), id));
     }
+
+    reverse(to_delete.begin(), to_delete.end());
+    for (auto it : to_delete)
+        ids_not_found_.erase(it);
+}
+
+int Sudoku::get_num_found() const { return ids_found_.size(); }
+
+int Sudoku::get_num_not_found() const { return ids_not_found_.size(); }
+
+int Sudoku::get_num_unchecked() const { return ids_unchecked_.size(); }
+
+void Sudoku::set_cell_value(
+    const SUDOKU_ID &id, const SUDOKU_VAL &val)
+{
+    if (val < 0 || val > S) {
+        cout << "Invalid value for a cell: " << val << endl;
+        return;
+    }
+    if (id < 0 || id >= S_MAP) {
+        cout << "Invalid id for a cell: " << id << endl;
+        return;
+    }
+    
+    if (map_[id] != 0 && val == 0) {
+        ids_found_.erase(find(begin(ids_found_), end(ids_found_), id));
+        ids_not_found_.push_back(id);
+        auto it = find(begin(ids_unchecked_), end(ids_unchecked_), id);
+        if (it != ids_unchecked_.end())
+            ids_unchecked_.erase(it);
+    } else if (map_[id] == 0 && val != 0) {
+        ids_not_found_.erase(find(begin(ids_not_found_), end(ids_not_found_), id));
+        ids_found_.push_back(id);
+        ids_unchecked_.push_back(id);
+    }
+    map_[id] = val;
+}
+
+SUDOKU_VAL Sudoku::get_cell_value(const SUDOKU_ID &id) const {
+    return map_[id];
 }
 
 void Sudoku::solve() {
 
-    // while (_ids_found.size()) {
-    //     print();
-    //     update_possible_values();
-    //     advanced_update();
-    //     fill_certain_values();
-    // }
+    while (ids_found_.size()) {
+        update_possible_values();
+        advanced_update();
+        fill_certain_values();
+    }
     print();
-    update_possible_values();
-    advanced_update();
-    print_possible_values();
-    fill_certain_values();
-    print_possible_values();
+
+    if (!ids_not_found_.empty())
+        print_possible_values();
+
+    // while (advanced_update) {
+    //     while (update_possible_values)
+    //         fill_certain_values();
+    // }
 
 #if LOGGING
     // print_possible_values();
 #endif
 }
+
+#endif  // SUDOKU_SOLVER__SUDOKU_HPP_
